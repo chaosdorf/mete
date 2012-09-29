f6fae8369e32a038e22e66ab1cc147743ab7242f
nomaster <nomaster@chaosdorf.de>
2012-09-30 01:18:12 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mete::Application.config.secret_token = '48eb56a7caee5ffb645221a26e85901d57ce45f4c74b54a1723d4062c7a60e29f0c46999e571e45a5562a13f0fcd20010137f019e37f12e88622a0d62b900a4f'
