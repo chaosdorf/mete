074e27827851d2bef49f47c4764499127fece4fd
nomaster <nomaster@chaosdorf.de>
2012-10-04 00:15:02 +0200

    deployment fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,22 @@ set :user, "http"
 role :app, "hyperion.chaosdorf.dn42"
 role :db, "hyperion.chaosdorf.dn42", :primary => true
 role :web, "hyperion.chaosdorf.dn42"
+after 'deploy:update', 'deploy:symlink_shared_paths', 'deploy:migrate', 'deploy:assets:precompile'
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :symlink_shared_paths do
+    run "mkdir -p #{shared_path}/db"
+    run "mkdir -p #{shared_path}/public/assets"
+    run "mkdir -p #{shared_path}/files"
+    run "mkdir -p #{shared_path}/log"
+    run "ln -fns #{shared_path}/db/production.sqlite3 #{release_path}/db/"
+    run "ln -fns #{shared_path}/public/assets #{release_path}/public/"
+    run "ln -fns #{shared_path}/files #{release_path}"
+    run "ln -fns #{shared_path}/log #{release_path}"
   end
 end
