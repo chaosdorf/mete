f3e279c92186776c16e6c14f7c390befe78c3a04
Maximilian Gaß <mxey@mxey.net>
2012-12-26 19:27:12 +0100

    Order users by name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :balance
   monetize :balance_cents
+  default_scope order('name')
+
   def deposit(amount)
     self.balance_cents += amount
     save
