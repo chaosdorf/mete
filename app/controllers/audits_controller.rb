02dd222c0bb62e96e31768652042be08810fb31a
Maximilian Gaß <mxey@mxey.net>
2013-04-20 16:58:47 +0200

    add filtering of audits by date

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,8 +1,15 @@
 class AuditsController < ApplicationController
   def index
-    @audits = Audit.all
-    @sum = Audit.sum(&:difference)
-    @payments_sum = Audit.payments.sum(&:difference).abs
-    @deposits_sum = Audit.deposits.sum(&:difference)
+    if params[:start_date] and params[:end_date]
+      @start_date = Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
+      @end_date = Date.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
+      @audits = Audit.where("created_at >= :start_date AND created_at <= :end_date", start_date: @start_date, end_date: @end_date)
+    else
+      @audits = Audit.scoped
+    end
+    
+    @sum = @audits.sum(&:difference)
+    @payments_sum = @audits.payments.sum(&:difference).abs
+    @deposits_sum = @audits.deposits.sum(&:difference)
   end
 end
