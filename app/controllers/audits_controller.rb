26451fef3c6076cc35c580e83eb716e59a6dc3ed
Maximilian Gaß <mxey@mxey.net>
2013-08-31 01:52:25 +0200

    Fix some deprecated ActiveRecord uses

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -5,11 +5,11 @@ class AuditsController < ApplicationController
       @end_date = Date.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
       @audits = Audit.where("created_at >= :start_date AND created_at <= :end_date", start_date: @start_date, end_date: @end_date)
     else
-      @audits = Audit.scoped
+      @audits = Audit.all
     end
     
-    @sum = @audits.sum(&:difference)
-    @payments_sum = @audits.payments.sum(&:difference).abs
-    @deposits_sum = @audits.deposits.sum(&:difference)
+    @sum = @audits.to_a.sum(&:difference)
+    @payments_sum = @audits.payments.to_a.sum(&:difference).abs
+    @deposits_sum = @audits.deposits.to_a.sum(&:difference)
   end
 end
