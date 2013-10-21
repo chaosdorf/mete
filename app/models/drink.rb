07e937cdc864d7cf9941225f070033b377c06ab6
nomaster <nomaster@chaosdorf.de>
2013-10-21 16:00:31 +0200

    normalize logo file names

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,4 +1,15 @@
 class Drink < ActiveRecord::Base
-  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
   validates_presence_of :name, :bottle_size, :price
+
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  before_post_process :normalize_filename
+
+  private
+
+  def normalize_filename
+    extension = File.extname(logo_file_name).downcase
+    self.logo.instance_write :file_name, "logo#{extension}"
+  end
+
 end
