62aa0dbeb932118c6b2295ed12bd49aa9aa49b65
Niklas Sombert <niklas@ytvwld.de>
2016-05-29 17:20:54 +0200

    no drinks in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,10 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @drinks = Drink.order("price DESC")
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
-      #format.json { render json: @drinks }
     end
   end
 
