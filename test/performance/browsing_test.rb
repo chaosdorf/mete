1321c01875239fca2f1e9fb28806676d30e1462f
Niklas Sombert <niklas@ytvwld.de>
2016-05-30 13:21:50 +0200

    require 'rails/perftest/action_dispatch' for tests

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'rails/performance_test_help'
+require 'rails/perftest/action_dispatch'
 
 class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
