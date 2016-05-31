e246b9fcd9863a7f94b58bc2a9e022c235674e4d
Niklas Sombert <niklas@ytvwld.de>
2016-05-31 17:47:50 +0200

    Show inactive users last in the list.
    Former-commit-id: be27b2031e0fea7982f5c6704e9670215967fc1c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(active: :desc, name: :asc).all
 
 
     respond_to do |format|
