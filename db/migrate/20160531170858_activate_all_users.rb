fe561e18f25237717d2d4f1c0d78798f4ebbf5d4
Niklas Sombert <niklas@ytvwld.de>
2016-05-31 17:02:59 +0200

    Allow users to be activated and deactivated.

---
+++ db/migrate/20160531170858_activate_all_users.rb
@@ -0,0 +1,12 @@
+class ActivateAllUsers < ActiveRecord::Migration
+#via http://api.rubyonrails.org/classes/ActiveRecord/Migration.html
+		def up
+			add_column :users, :active, :boolean, default: true
+			User.all.each { |user| user.active = true }
+		end
+
+		def down
+			remove_column :users, :active
+		end
+
+	end
