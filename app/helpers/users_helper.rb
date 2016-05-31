86b5b718a48501f4394fc26a6f8d793bff416402
Niklas Sombert <niklas@ytvwld.de>
2016-05-31 17:06:41 +0200

    Show disabled users with a grayed-out logo.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def gravatar(user)
     if user.respond_to? :email
-      gravatar_image_tag(user.email, :gravatar => { :default => 'https://assets.github.com/images/gravatars/gravatar-140.png' })
+      gravatar_image_tag user.email, :gravatar => { :default => 'https://assets.github.com/images/gravatars/gravatar-140.png' }, class: user.active? ? "" : "disabled"
     else
-      gravatar_image_tag
+      gravatar_image_tag class: user.active? ? "" : "disabled"
     end
   end
 end
