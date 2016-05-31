b392c0d20763518b2db47f441a37b68cd6dbd590
Niklas Sombert <niklas@ytvwld.de>
2016-05-31 14:30:06 +0200

    Fix show drinks in audits.

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,6 +1,12 @@
 class Audit < ActiveRecord::Base
   default_scope ->{ order('created_at DESC') }
-  
+
   scope :deposits, ->{ where('difference > 0') }
   scope :payments, ->{ where('difference < 0') }
+
+  def as_json(options)
+    h = super(options)
+    h[:drink] = drink
+   return h
+  end
 end
