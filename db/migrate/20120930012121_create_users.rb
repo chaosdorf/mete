aa82fe0d7d7c8c69f89121c9668f6177ab9d3fc8
Niklas Sombert <niklas@ytvwld.de>
2016-05-31 13:12:09 +0200

    Future-proofing our db migrations for Rails 5.

--- db/migrate/20120930012121_create_users.rb
+++ db/migrate/20120930012121_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.integer :balance_cents
-      t.timestamps
+      t.timestamps null: true
     end
   end
 end
