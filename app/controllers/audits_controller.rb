21a3fd8d1ad073c2fa81b219295c370f536ed153
Niklas Sombert <niklas@ytvwld.de>
2016-06-01 11:42:47 +0200

    Simplify the audits controller a bit more.

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -5,7 +5,7 @@ class AuditsController < ApplicationController
     if params[:start_date] and params[:end_date]
       @start_date = parse_date params[:start_date]
       @end_date = parse_date params[:end_date]
-      @audits = Audit.where("created_at >= :start_date AND created_at <= :end_date", start_date: @start_date, end_date: @end_date)
+      @audits = Audit.where(created_at: (@start_date..@end_date))
     else
       @audits = Audit.all
     end
