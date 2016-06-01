24263a6c755dca798e2e85660ed57ef4c2da12aa
Niklas Sombert <niklas@ytvwld.de>
2016-06-01 14:52:29 +0200

    Remove User.balance_sum.
    It wasn't used.
    
    
    Former-commit-id: 415724bcf83a32d33b3812ab63f9e9c82b0f2944

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ActiveRecord::Base
     Audit.create! difference: user.balance - user.balance_was, drink: @purchased_drink.nil? ? 0 : @purchased_drink.id
   end
 
-  def self.balance_sum
-    self.sum(:balance)
-  end
-
   def deposit(amount)
     self.balance += amount
     save!
