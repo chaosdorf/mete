a7434fa724e424dbf7c374ddaed625efad7bf7a0
Niklas Sombert <niklas@ytvwld.de>
2016-06-03 13:18:58 +0200

    Update db schema.
    Former-commit-id: 409dc4f032bbdac78cf4779a06e040b37233fddb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531170858) do
+ActiveRecord::Schema.define(version: 20160531175554) do
 
   create_table "audits", force: :cascade do |t|
     t.datetime "created_at"
