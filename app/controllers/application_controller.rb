eb9bd3c9d6457135dcf74fddecbecea2bee8c54d
Niklas Sombert <niklas@ytvwld.de>
2017-02-27 13:48:05 +0100

    Raven: Use to_unsafe_h instead of to_hash whichi is deprecated.
    Former-commit-id: cf4f069b658e8d4c705cd0a42a8d64a8e3c0c994

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
-    Raven.extra_context(params: params.to_hash, url: request.url)
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
