21fdf12dcfb56b8e121f49019f079c53fcb9429e
Niklas Sombert <niklas@ytvwld.de>
2017-02-28 16:26:41 +0100

    Add unit tests.
    Former-commit-id: b51d940b8b7a87b11353ff764ecddd828c04faf5

--- test/unit/drink_test.rb
+++ test/unit/drink_test.rb
@@ -1,7 +1,73 @@
 require 'test_helper'
 
 class DrinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    drink = Drink.new
+    drink.name = nil
+    drink.bottle_size = 0.5
+    drink.price = 1.5
+    assert_not drink.save, "Saved without a name"
+  end
+  
+  test "should not save without bottle_size" do
+    drink = Drink.new
+    drink.name = "Test"
+    drink.bottle_size = nil
+    drink.price = 1.5
+    assert_not drink.save, "Saved without bottle_size"
+  end
+  
+  test "should not save without price" do
+    drink = Drink.new
+    drink.name = "Test"
+    drink.bottle_size = 0.5
+    drink.price = nil
+    assert_not drink.save, "Saved without a price"
+  end
+  
+  test "should save" do
+    drink = Drink.new
+    drink.name = "Test"
+    drink.bottle_size = 0.5
+    drink.price = 1.5
+    assert drink.save, "Failed to save"
+  end
+  
+  test "should have attributes" do
+    d = drinks(:one)
+    assert_respond_to d, :id, "id missing"
+    assert_respond_to d, :name, "name missing"
+    assert_respond_to d, :bottle_size, "bottle_size missing"
+    assert_respond_to d, :caffeine, "caffeine missing"
+    assert_respond_to d, :price, "price missing"
+    assert_respond_to d, :logo_file_name, "logo_file_name missing"
+    assert_respond_to d, :created_at, "created_at missing"
+    assert_respond_to d, :updated_at, "updated_at missing"
+    assert_respond_to d, :logo_content_type, "logo_content_type missing"
+    assert_respond_to d, :logo_file_size, "logo_file_size missing"
+    assert_respond_to d, :logo_updated_at, "logo_updated_at missing"
+    assert_respond_to d, :active, "active missing"
+  end
+  
+  test "should export to json" do
+    assert_kind_of Hash, drinks(:one).as_json({}), "Failed to export to json"
+  end
+  
+  test "should correctly export to json" do
+    j = drinks(:one).as_json({})
+    assert j.key?('id'), "Failed to export id"
+    assert j.key?('name'), "Failed to export name"
+    assert j.key?('bottle_size'), "Failed to export bottle size"
+    assert j.key?('caffeine'), "Failed to export caffeine"
+    assert j.key?('price'), "Failed to export price"
+    assert j.key?('donation_recommendation'), "Failed to export donation recommendation"
+    assert j.key?('logo_file_name'), "Failed to export logo_file_name"
+    assert j.key?('created_at'), "Failed to export created_at"
+    assert j.key?('updated_at'), "Failed to export updated_at"
+    assert j.key?('logo_content_type'), "Failed to export logo_content_type"
+    assert j.key?('logo_file_size'), "Failed to export logo_file_size"
+    assert j.key?('logo_updated_at'), "Failed to export logo_updated_at"
+    assert j.key?('active'), "Failed to export active"
+    assert j.key?('logo_url'), "Failed to export logo_url"
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,80 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    user = User.new
+    assert_not user.save, "Saved the user without a name"
+  end
+  
+  test "should save" do
+    user = User.new
+    user.name = "test"
+    assert user.save, "Failed to save the user with a name"
+  end
+  
+  test "should deposit" do
+    assert users(:one).deposit(rand(500)), "Failed to deposit money"
+  end
+  
+  test "depositing should increase balance" do
+    user = users(:one)
+    amount = rand(500)
+    balance_was = user.balance
+    user.deposit amount
+    assert_equal balance_was + amount, user.balance, "Depositing money didn't increase the balance"
+  end
+  
+  test "should pay" do
+    assert users(:one).payment(rand(500)), "Failed to pay"
+  end
+  
+  test "payment should decrease balance" do
+    user = users(:one)
+    amount = rand(500)
+    balance_was = user.balance
+    user.payment amount
+    assert_equal balance_was - amount, user.balance, "Payment didn't decrease the balance"
+  end
+  
+  test "should buy a drink" do
+    assert users(:one).buy(drinks(:one)), "Failed to buy"
+  end
+  
+  test "buying should decrease balance" do
+    user = users(:one)
+    drink = drinks(:one)
+    balance_was = user.balance
+    user.buy drink
+    assert_equal balance_was - drink.price, user.balance, "Buying didn't decrease the balance"
+  end
+  
+  test "should have attributes" do
+    u = users(:one)
+    assert_respond_to u, :id, "id missing"
+    assert_respond_to u, :name, "name missing"
+    assert_respond_to u, :email, "email missing"
+    assert_respond_to u, :created_at, "created_at missing"
+    assert_respond_to u, :updated_at, "updated_at missing"
+    assert_respond_to u, :balance, "balance missing"
+    assert_respond_to u, :active, "active missing"
+    assert_respond_to u, :audit, "audit missing"
+    assert_respond_to u, :redirect, "redirect missing"
+  end
+  
+  test "should export to json" do
+    assert_kind_of Hash, users(:one).as_json({}), "Failed to export to json"
+  end
+  
+  test "should correctly export to json" do
+    j = users(:one).as_json({})
+    assert j.key?('id'), "Failed to export id"
+    assert j.key?('name'), "Failed to export name"
+    assert j.key?('email'), "Failed to export email"
+    assert j.key?('created_at'), "Failed to export created_at"
+    assert j.key?('updated_at'), "Failed to export updated_at"
+    assert j.key?('balance'), "Failed to export balance"
+    assert j.key?('active'), "Failed to export active"
+    assert j.key?('audit'), "Failed to export audit"
+    assert j.key?('redirect'), "Failed to export redirect"
+  end
 end
