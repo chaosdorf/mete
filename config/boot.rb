469f70f763c3538f6043515bda9fa669ee33a40d
Niklas Sombert <niklas@ytvwld.de>
2017-09-30 14:24:23 +0200

    Load the config on startup.

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# load the config
+require 'yaml'
+APP_CONFIG = YAML.load_file(File.expand_path('../../var/config.yaml', __FILE__))
