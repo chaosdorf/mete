d945a0fe19b9a43a756e3cc1f24aab0f0d17f99f
marudor <marudor@marudor.de>
2018-03-03 17:25:05 +0100

    Filter by initia in user list

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,13 @@
+var allowedHashFilter = '0abcdefghijklmnopqrstuvwxyz';
+function hashchange() {
+    var filterChar = location.hash.charAt(1);
+    if (!filterChar) {
+        filterChar = allowedHashFilter.charAt(Math.floor(Math.random() * allowedHashFilter.length));
+    }
+    var filter = '.user:not(.' + filterChar + ')';
+    $('.user').show();
+    $(filter).hide();
+}
+
+$(window).on('hashchange', hashchange);
+hashchange();
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -130,7 +130,7 @@ class UsersController < ApplicationController
       flash[:warning] = "Your balance is below zero. Remember to compensate as soon as possible."
     end
     warn_user_if_audit
-    no_resp_redir @user.redirect ? users_url : @user
+    no_resp_redir @user.redirect ? users_url + '/#' + @user.initial : @user
   end
 
   def user_params
