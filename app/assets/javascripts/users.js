e8524def3439cb835f6fab3239f1af1271ad028e
marudor <marudor@marudor.de>
2018-03-03 18:48:57 +0100

    add editorconfig to enforce EOF newline

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,12 +1,12 @@
 var allowedHashFilter = '0abcdefghijklmnopqrstuvwxyz';
 function hashchange() {
-    var filterChar = location.hash.charAt(1);
-    if (!filterChar) {
-        filterChar = allowedHashFilter.charAt(Math.floor(Math.random() * allowedHashFilter.length));
-    }
-    var filter = '.user:not(.' + filterChar + ')';
-    $('.user').show();
-    $(filter).hide();
+  var filterChar = location.hash.charAt(1);
+  if (!filterChar) {
+    filterChar = allowedHashFilter.charAt(Math.floor(Math.random() * allowedHashFilter.length));
+  }
+  var filter = '.user:not(.' + filterChar + ')';
+  $('.user').show();
+  $(filter).hide();
 }
 
 $(window).on('hashchange', hashchange);
