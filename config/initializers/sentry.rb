717547ff80375e7abdc8f31776fb467d7f703ba9
Niklas Sombert <niklas@ytvwld.de>
2019-07-09 22:23:19 +0200

    :sparkles: Load SENTRY_DSN from a secret if possible

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,7 @@ require "raven"
 
 Raven.configure do |config|
   config.environments = ['production']
+  if File.file?("/run/secrets/SENTRY_DSN")
+    config.dsn = File.read("/run/secrets/SENTRY_DSN").strip
+  end
 end
