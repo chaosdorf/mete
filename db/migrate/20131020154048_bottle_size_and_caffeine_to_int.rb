a7ac66038f89a2ba96c6ed7c4d95082262f97e56
Niklas Sombert <niklas@ytvwld.de>
2020-01-16 15:01:38 +0100

    :bug: Adjust old migrations to work with Postgres (closes #79)
    And everything still works on SQLite.

--- db/migrate/20131020154048_bottle_size_and_caffeine_to_int.rb
+++ db/migrate/20131020154048_bottle_size_and_caffeine_to_int.rb
@@ -1,6 +1,6 @@
 class BottleSizeAndCaffeineToInt < ActiveRecord::Migration[4.2]
   def change
-    change_column :drinks, :bottle_size, :integer
-    change_column :drinks, :caffeine, :integer
+    change_column :drinks, :bottle_size, :integer, using: 'bottle_size::integer'
+    change_column :drinks, :caffeine, :integer, using: 'caffeine::integer'
   end
 end
