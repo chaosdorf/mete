82dbbd17a16c6753422c76c1ac1f91d97e541514
Niklas Sombert <niklas@ytvwld.de>
2021-03-06 14:24:19 +0100

    :sparkles: Only filter users by initial if there are many users
    This closes #77.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,20 @@
-var allowedHashFilter = '0abcdefghijklmnopqrstuvwxyz';
-function hashchange() {
-  var filterChar = location.hash.charAt(1);
-  if (!filterChar) {
-    filterChar = allowedHashFilter.charAt(Math.floor(Math.random() * allowedHashFilter.length));
+// filter users by initial
+// only do this if we have many users
+if ($('.user').length >= 50) {
+  var allowedHashFilter = '0abcdefghijklmnopqrstuvwxyz';
+  function hashchange() {
+    var filterChar = location.hash.charAt(1);
+    if (!filterChar) {
+      filterChar = allowedHashFilter.charAt(Math.floor(Math.random() * allowedHashFilter.length));
+    }
+    var filter = '.user:not(.' + filterChar + ')';
+    $('.user').show();
+    $(filter).hide();
   }
-  var filter = '.user:not(.' + filterChar + ')';
-  $('.user').show();
-  $(filter).hide();
-}
 
-$(window).on('hashchange', hashchange);
-hashchange();
+  $(window).on('hashchange', hashchange);
+  hashchange();
+} else {
+  $('.user').show();
+  $('.user-filter').hide();
+}
