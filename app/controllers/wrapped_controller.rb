6ab88b7566bc73d94c2c763b0364bd9ba641319d
Niklas Sombert <niklas@ytvwld.de>
2021-12-07 22:31:26 +0100

    :sparkles: Add most bought drink to wrapped

--- app/controllers/wrapped_controller.rb
+++ app/controllers/wrapped_controller.rb
@@ -13,6 +13,10 @@ class WrappedController < ApplicationController
     @year = Integer(params[:id])
     audits = audits_for(@user, @year)
     @empty = audits.none?
+    unless @empty
+      @most_bought_drink = most_bought_drink(audits, @year)
+    end
+    
     # wrapped.html.haml
   end
   
@@ -22,5 +26,15 @@ class WrappedController < ApplicationController
     Audit
       .where(user: user)
       .where(created_at: Date.new(year, 01, 01)..Date.new(year, 12, 31))
+  end
+  
+  def most_bought_drink(audits, year)
+    drink_id, count = audits.reorder(nil).group(:drink).count.max_by { |_, v| v }
+    drink = Drink.find(drink_id)
+    user_more = audits_for(User.where(audit: true), year)
+                .where(drink: drink).reorder(nil).group(:user).count
+                .select { |_, v| v > count }
+                .size
+    { drink: drink, count: count, user_more: user_more }
   end
 end
