8e4601680cecbd7fae816ccb4a302374060e03c8
Niklas Sombert <niklas@ytvwld.de>
2021-12-08 22:25:29 +0100

    :sparkles: Add stats about caffeine consumption

--- app/controllers/wrapped_controller.rb
+++ app/controllers/wrapped_controller.rb
@@ -15,6 +15,7 @@ class WrappedController < ApplicationController
     @empty = audits.none?
     unless @empty
       @most_bought_drink = most_bought_drink(audits, @year)
+      @caffeine = caffeine(audits)
     end
     
     # wrapped.html.haml
@@ -35,5 +36,35 @@ class WrappedController < ApplicationController
                 .select { |_, v| v > count }
                 .size
     { drink: drink, count: count, user_more: user_more }
+  end
+  
+  def caffeine(audits)
+    total = audits.joins(:drink).sum(:caffeine)
+    if total.positive?
+      # for humans: overdose: 1000 mg, intoxication: 5000 mg
+      would_kill_kg = total / 192.0  # lethal dosage (mg) per kg
+      puts would_kill_kg
+      would_kill = case would_kill_kg
+                   when 0..0.025 then nil
+                   when 0.025..0.3 then 'hamster'
+                   when 0.3..0.4 then 'squirrel'
+                   when 0.4..0.9 then 'rat'
+                   when 0.9..2.5 then 'guinea pig'
+                   when 2.5..5 then 'lemur'
+                   when 5..9 then 'cat'
+                   when 9..13 then 'koala'
+                   when 13..23 then 'coyote'
+                   when 23..55 then 'lynx'
+                   when 55..81 then 'capybara'
+                   when 81..101 then 'jaguar'
+                   when 101..140 then 'reindeer'
+                   when 140..175 then 'gorilla'
+                   when 175..278 then 'lion'
+                   when 278..368 then 'bear'
+                   when 368..540 then 'moose'
+                   when 540..Float::INFINITY then 'bison'
+                   end
+    end
+    { total: total, would_kill: would_kill }
   end
 end
