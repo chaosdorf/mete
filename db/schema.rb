a4fe1bdc0b9004c10c789a91cb0cff87568e2f03
marudor <marudor@marudor.de>
2022-07-30 10:53:17 +0200

    ⬆ Update Dependencies, including rails 6->7 and ruby 2.7->3.1 (#123)
    * ⬆ Update Dependencies, including rails 6->7 and ruby 2.7->3.1
    
    * run github workflow on PRs
    
    * fix gemfile.lock
    
    * remove performance tests as the gem does not support rails 7
    
    * only run build github action on main branch

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_09_29_201659) do
-
+ActiveRecord::Schema[7.0].define(version: 2017_09_29_201659) do
   create_table "audits", force: :cascade do |t|
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.decimal "difference", precision: 20, scale: 2, default: "0.0"
     t.integer "drink"
     t.integer "user"
@@ -29,19 +28,19 @@ ActiveRecord::Schema.define(version: 2017_09_29_201659) do
     t.integer "caffeine"
     t.decimal "price", precision: 20, scale: 2, default: "0.0"
     t.string "logo_file_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.string "logo_content_type"
     t.integer "logo_file_size"
-    t.datetime "logo_updated_at"
+    t.datetime "logo_updated_at", precision: nil
     t.boolean "active", default: true
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.decimal "balance", precision: 20, scale: 2, default: "0.0"
     t.boolean "active", default: true
     t.boolean "audit", default: false
