a7aaf161874ce7f573d3706dc516843c6419a472
Niklas Sombert <niklas@ytvwld.de>
2025-05-16 22:14:24 +0200

    :arrow_up: Update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_153705) do
+ActiveRecord::Schema[7.1].define(version: 2023_02_12_153705) do
   create_table "audits", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.decimal "difference", precision: 20, scale: 2, default: "0.0"
